eaf549d7d3102ddc1ab6398a3f791684afc6aaed
adamk-ai <adamkiss599@gmail.com>
2025-12-01 19:10:22 +0100

    Updated Team.tsx

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -11,7 +11,7 @@ export function Team() {
     position: t('team.ceo.position'),
     specialization: t('team.ceo.specialization'),
     description: t('team.ceo.description'),
-    image: '/aki-website-photo.png',
+    image: '/aki-website-photo copy.png',
     imagePosition: 'center 20%',
     email: 'akos.horvath@office.4hsafety.com',
     linkedin: 'https://linkedin.com/in/akos-horvath'
